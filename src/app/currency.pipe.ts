815cb32b0aa6b27b2e89732b610434fd4ddb42b2
KripeshM <mkripeshmullachery@gmail.com>
2023-03-25 20:44:10 +0530

    currency converter

---
+++ src/app/currency.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+interface CurrencySymbol {
+  [key: string]: string;
+}
+
+@Pipe({
+  name: 'currency'
+})
+export class CurrencyPipe implements PipeTransform {
+  transform(value: number, currencyCode: string): string {
+    const currencySymbol: CurrencySymbol = {
+      USD: '$',
+      EUR: '€',
+      GBP: '£',
+      JPY: '¥',
+      AUD: 'A$'
+    };
+
+    return `${currencySymbol[currencyCode as keyof CurrencySymbol]}${value.toFixed(2)}`;
+  }
+}
